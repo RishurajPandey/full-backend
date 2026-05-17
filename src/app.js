a023c9daee9e78f4205db2075d0da1d7fe50c8cd
Rishuraj Pandey <rishu04112004@gmail.com>
2026-05-17 12:14:50 +0530

    add register contoller+fixes

--- src/app.js
+++ src/app.js
@@ -15,4 +15,16 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+
+import userRouter from './routes/user.routes.js';
+
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)  
+// http://localhost:5000/api/v1/users/register
+// http://localhost:5000/api/v1/users/login
+
+
 export { app }
